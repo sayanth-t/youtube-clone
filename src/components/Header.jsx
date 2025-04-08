1f187b8b7560ef4d202f746a035bfcfd9d1835a1
sayanth-t <sayanth10krm@gmail.com>
2025-04-08 12:22:55 +0530

    feat : Debounsing search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,54 @@
 import { RxHamburgerMenu } from 'react-icons/rx';
-import { youtubeLogo } from '../utils/constants';
+import { youtubeLogo, YT_SEARCH_SUGGESTION_API } from '../utils/constants';
 import { FaPlus } from 'react-icons/fa6';
 import { IoIosNotificationsOutline } from 'react-icons/io';
 import { CgProfile } from 'react-icons/cg';
 import { CiSearch } from 'react-icons/ci';
 import { useDispatch } from 'react-redux';
 import { toggleSideBar } from '../utils/appSlice';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { GrClose } from "react-icons/gr";
 
 const Header = () => {
+
+  const [ searchQuery , setSearchQuery ] = useState("") ;
+  const [ searchSuggestion , setsearchSuggestion ] = useState([]) ;
+
   const dispatch = useDispatch();
 
   const handleSideBar = (e) => {
     e.preventDefault();
     dispatch(toggleSideBar());
   };
+
+  const getSuggestinos = async () => {
+    try {
+      const res = await axios.get(YT_SEARCH_SUGGESTION_API+searchQuery) ;
+      setsearchSuggestion(res.data[1]) ;
+
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  const handleClickOutside = () => {
+    try {
+      setsearchSuggestion([]) ;
+    } catch (err) {
+      console.log(err.message) ;
+    }
+  }
+
+  useEffect(()=>{
+    const suggestinTimer = setTimeout(()=>getSuggestinos(),200) ;
+
+    document.addEventListener("mousedown",handleClickOutside)
+    
+    return ()=> {
+      clearTimeout(suggestinTimer) ;
+    }
+  },[searchQuery])
 
   return (
     <div className="w-full flex flex-wrap justify-between items-center px-4 sm:px-6 lg:px-10 py-3 gap-4 bg-white shadow-md mb-5">
@@ -33,15 +68,45 @@ const Header = () => {
       </div>
 
       {/* Middle Section */}
-      <div className="flex items-center flex-grow max-w-md mx-auto w-full">
-        <input
-          type="text"
-          placeholder="Search"
-          className="flex-grow border border-slate-300 rounded-l-full px-4 py-2 text-sm sm:text-base focus:outline-none"
-        />
+      <div className="relative flex items-center flex-grow max-w-md mx-auto w-full">
+      <div className="relative w-full flex-grow max-w-md mx-auto">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full border border-slate-300 rounded-l-full px-4 pr-10 py-2 text-sm sm:text-base focus:outline-none"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+          {/* Close Icon */}
+          {searchQuery && (
+            <GrClose
+              onClick={() => setSearchQuery("")}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer text-sm sm:text-base hover:text-black"
+            />
+          )}
+        </div>
+
+
         <div className="border border-slate-300 border-l-0 bg-slate-100 rounded-r-full px-4 flex items-center justify-center h-10 cursor-pointer">
           <CiSearch className="text-xl text-gray-600" />
         </div>
+
+        {/* Suggestions Dropdown */}
+        { searchSuggestion.length !== 0 && (
+          <div className="absolute top-full left-0 w-full bg-white border border-gray-200 rounded-md shadow-md mt-1 z-10">
+            { searchSuggestion
+                .map((suggestion, index) => (
+                    <div
+                      key={index}
+                      className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
+                      onClick={() => setSearchQuery(suggestion)}
+                    >
+                      {suggestion}
+                    </div>
+                ))}
+          </div>
+        )}
       </div>
 
       {/* Right Section */}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,4 @@ export const buttonMenus = ["All","Javascript","Poadcasts","Computer Programming
 export const API_KEY = "AIzaSyAOe-Z9XUODM87pSFoyAZ_B1Qz8KegUhMg" 
 export const YT_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=AIzaSyAOe-Z9XUODM87pSFoyAZ_B1Qz8KegUhMg "
 export const YT_BASE_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&"
+export const YT_SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
